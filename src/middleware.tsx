561da332f63564c545bf5a325e3244caac88a240
FacundoPalomanes <facpalomanes@gmail.com>
2025-05-09 15:39:01 -0300

    test: testing

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -20,7 +20,8 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-    matcher: ['/', '/settings', '/add']
+    // matcher: ['/', '/settings', '/add']
+    matcher: []
 }
 
 // para hacer todas las rutas adentro de una ruta es '/carpeta/:path*' eso significa q todas las rutas anidadas de esa carpeta estan protegidas por el middleware`
